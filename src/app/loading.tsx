778bdc5c69f0a7c7172f8adb9a5b543e720bd610
Arifur Rahaman <arifur.rahaman.siflu@gmail.com>
2026-02-04 22:12:55 +0600

    Update skeleton

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import HeroPartSkeleton from "@/components/specific/HeroPartSkeleton";
+import SearchBoxSkeleton from "@/components/specific/SearchBoxSkeleton";
 import SeatInfoSkeleton from "@/components/specific/SeatInfoSkeleton";
+import SeatCandidatesResultSkeleton from "@/components/specific/SeatCandidatesResultSkeleton";
 
 export default function HomePageSkeleton() {
   return (
     <div className="lg:mb-14 mb-10">
-      <SeatInfoSkeleton className="lg:mt-14 mt-10" />
+      <div className="relative z-10">
+        <HeroPartSkeleton />
+        <div className="hidden lg:block">
+          <SearchBoxSkeleton />
+        </div>
+      </div>
+      <div className="relative z-0">
+        <div className="lg:hidden">
+          <SearchBoxSkeleton />
+        </div>
+        <SeatInfoSkeleton className="lg:mt-14 mt-10" />
+        <div className="space-y-10 mt-10">
+          <SeatCandidatesResultSkeleton />
+          <SeatCandidatesResultSkeleton />
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/pools/[seatNo]/loading.tsx
+++ src/app/pools/[seatNo]/loading.tsx
@@ -1,5 +1,23 @@
+import React from "react";
+import HeroPartSkeleton from "@/components/specific/HeroPartSkeleton";
+import SearchBoxSkeleton from "@/components/specific/SearchBoxSkeleton";
 import SeatCandidatesResultSkeleton from "@/components/specific/SeatCandidatesResultSkeleton";
 
 export default function PoolLoading() {
-  return <SeatCandidatesResultSkeleton />;
+  return (
+    <div className="lg:mb-14 mb-10">
+      <div className="relative z-10">
+        <HeroPartSkeleton />
+        <div className="hidden lg:block">
+          <SearchBoxSkeleton />
+        </div>
+      </div>
+      <div className="relative z-0">
+        <div className="lg:hidden">
+          <SearchBoxSkeleton />
+        </div>
+        <SeatCandidatesResultSkeleton />
+      </div>
+    </div>
+  );
 }
